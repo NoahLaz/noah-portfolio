f8928fe5d01c84ca8d593e0ab984a0cbd9a25b91
Nouh lazreq <nouh.salama@gmail.com>
2024-10-19 00:24:31 +0100

    Fixed env variables

--- src/fireBase.js
+++ src/fireBase.js
@@ -6,12 +6,13 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 
-const apiKey = import.meta.REACT_APP_FIREBASE_API_KEY;
-const authDomain = import.meta.REACT_APP_FIREBASE_AUTH_DOMAIN;
-const projectId = import.meta.REACT_APP_FIREBASE_PROJECTID;
-const storageBucket = import.meta.REACT_APP_FIREBASE_STORAGE_BUCKET;
-const messagingSenderId = import.meta.REACT_APP_FIREBASE_MESSAGING_SENDERID;
-const appId = import.meta.REACT_APP_FIREBASE_APPID;
+const apiKey = import.meta.VITE_FIREBASE_API_KEY;
+const authDomain = import.meta.VITE_FIREBASE_AUTH_DOMAIN;
+const projectId = import.meta.VITE_FIREBASE_PROJECTID;
+const storageBucket = import.meta.VITE_FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = import.meta.VITE_FIREBASE_MESSAGING_SENDERID;
+const appId = import.meta.VITE_FIREBASE_APPID;
+const databaseURL = import.meta.VITE_FIREBASE_DATABASE_URL;
 
 const firebaseConfig = {
   apiKey,
@@ -20,7 +21,7 @@ const firebaseConfig = {
   storageBucket,
   messagingSenderId,
   appId,
-  databaseURL: `https://react-portfolio-9e76d-default-rtdb.europe-west1.firebasedatabase.app/`,
+  databaseURL,
 };
 
 // Initialize Firebase
